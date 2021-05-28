aeef968b1e29a7017e26f68072e1d7e258a0c4c3
SuperStarDev51 <v.eridicaldeveloper@gmail.com>
2021-05-28 17:16:09 +0300

    Making avatar and submenus

--- src/layouts/components/menu/vertical-menu/additional-menu/index.js
+++ src/layouts/components/menu/vertical-menu/additional-menu/index.js
@@ -6,7 +6,7 @@ import { Icon } from '@components';
 import { useOutsideClick } from '@src/utilities';
 import navigationConfig from "../../../../../configs/navigationConfig";
 import './index.scss';
-
+import classnames from "classnames"
 
 const AdditionalMenu = ({ events }) => {
 	const [isOpen, setIsOpen] = React.useState(false);
@@ -20,31 +20,33 @@ const AdditionalMenu = ({ events }) => {
 	const activeEventId = events.filter((event) => event.preview)[0] ? events.filter((event) => event.preview)[0].id : null;
 	const additionalNavItems = navigationConfig(direction, activeEventId).filter(item => item.type === 'additionalMenu');
 
+	const avatar_style = {width: "3rem", height:"3rem" , postion:'absolute',borderRadius: "50%", marginLeft:"40px" }
 	return (
-		<div ref={ref} className="additional-menu">
+		<div ref={ref} className="additional-menu" >
 			{isOpen && (
 				<div className="additional-menu__dropdown">
-					{additionalNavItems.map(item => (
-						<Link
-							to={item.navLink}
-							onClick={() => setIsOpen(!isOpen)}
-							className="additional-menu__item"
-							data-tip = {item.tooltip}
-						>
-							
-							<FormattedMessage id={item.title} />
-						</Link>
-					))}
+					<ul className="navigation-main">
+						{additionalNavItems.map(item => (
+							<li
+								className={classnames("nav-item R")}>
+								<Link
+									to={item.navLink}
+									onClick={() => setIsOpen(!isOpen)}
+									className="additional-menu__item"
+									data-tip = {item.tooltip}
+								>
+									<FormattedMessage id={item.title} />
+								</Link>
+							</li>
+						))}
+					</ul>
 				</div>
 			)}
 			<div
 				className="additional-menu__icon"
 				onClick={()=> setIsOpen(!isOpen)}
 			>
-				<Icon
-					src={"../../../assets/icons/navbar/menu.svg"}
-					style={{margin: '1.5rem auto 0.5rem',height: '3rem', width:'3rem'}}
-				/>
+				<img src="../../../assets/img/Eliran.jpeg" style={avatar_style}></img>
 			</div>
 		</div>
 	)
