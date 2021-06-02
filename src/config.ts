55508d26d52f11947424da10217e13308ffe92cd
SuperStarDev51 <v.eridicaldeveloper@gmail.com>
2021-06-02 04:55:58 +0300

    remain only synopsis env in client

--- src/config.ts
+++ src/config.ts
@@ -5,8 +5,6 @@ interface Config {
 
   export const config: Config = {
     // "ipServer": "http://localhost:3000",
-    // "ipServer": "http://imgnserverdev-env.eba-nedy53v3.us-east-1.elasticbeanstalk.com",
     "ipServer": "http://synopsis-env.eba-jer6apbt.us-east-2.elasticbeanstalk.com",
-    //"ipServer": "https://api.imgn.co",
     "iconsPath": '../../../assets/icons/'
   }
