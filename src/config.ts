d9fc1efe79581fee4dec454f9345ed9403110148
SuperStarDev51 <v.eridicaldeveloper@gmail.com>
2021-06-04 18:26:16 +0300

    setting for local

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ interface Config {
 }
 
   export const config: Config = {
-    // "ipServer": "http://localhost:3000",
-    "ipServer": "http://synopsis-env.eba-jer6apbt.us-east-2.elasticbeanstalk.com",
+    "ipServer": "http://localhost:3000",
+    // "ipServer": "http://synopsis-env.eba-jer6apbt.us-east-2.elasticbeanstalk.com",
     "iconsPath": '../../../assets/icons/'
   }
