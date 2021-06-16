5297e71200ae3a63fe844961c8b31fb493801b1a
Abdulwahab <abdul.wahab@bigosoft.co.uk>
2021-06-16 09:52:09 +0500

    debugged the code for not making any calls

--- src/containers/profile/action/index.ts
+++ src/containers/profile/action/index.ts
@@ -2,26 +2,26 @@ import axios from 'axios';
 import { config } from '../../../config';
 
 export const updateMyProfile = (userData: any) => new Promise((resolve, reject) => {
-	console.log(userData)
 	axios.post(`${config.ipServer}/imgn/api/v1/user/update_profile`, {
 	userData,
-	})
-		.then(res =>
-			console.log(res)
-
-	).catch(err => reject(err))
+	}).then(res => console.log(res)).catch(err => reject(err))
 });
 
-export const uploadProfileImage = (userData: any) => new Promise((resolve, reject) => {
-			const data = new FormData();
-			console.log(userData);
-	    	data.append('file', userData);
-			data.append('user_id', '2');
-	axios.post(`${config.ipServer}/imgn/api/v1/project/imgfile/uploadProfileIMG`,
-		data
-	)
-		.then(res =>
-			console.log(res)
 
-	).catch(err => reject(err))
-});
+{/*
+	Ignore this function
+*/}
+
+// export const uploadProfileImage = (userData: any) => new Promise((resolve, reject) => {
+// 			const data = new FormData();
+// 			console.log(userData);
+// 	    	data.append('file', userData);
+// 			data.append('user_id', '2');
+// 	axios.post(`${config.ipServer}/imgn/api/v1/project/imgfile/uploadProfileIMG`,
+// 		data
+// 	)
+// 		.then(res =>
+// 			console.log(res)
+
+// 	).catch(err => reject(err))
+// });
